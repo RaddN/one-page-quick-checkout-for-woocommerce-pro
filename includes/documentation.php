c1097b344d34aebe1841f9e4385cdde800e4b0a5
RaddN <hossainraihan02@gmail.com>
2025-11-04 15:29:03 +0600

    fixed issues when any variation selected for variation product

--- includes/documentation.php
+++ includes/documentation.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) exit;
 /**
  * Renders the Floating Cart documentation page
  * 
- * @since 1.1.0
+ * @since 1.1.0.5
  * @return void
  */
 function onepaqucpro_cart_documentation()
