55e5f4a3967e3adfeba79ff923874e4225451c68
RaddN <hossainraihan02@gmail.com>
2026-03-30 15:41:33 +0600

    Version: 1.1.7.3

--- includes/blocks/plugincy-cart-blocks.php
+++ includes/blocks/plugincy-cart-blocks.php
@@ -299,7 +299,7 @@ function onepaqucpro_wc_checkout_block_render($attributes = array()) {
         'rmenupro-cart-block-style',
         false, // No actual CSS file
         array(), // No dependencies
-        '1.1.7.2.6' // Version
+        '1.1.7.3.6' // Version
     );
     
     // Now enqueue it
