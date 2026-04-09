198e7b1b06f23012b181fc97d638a89ccb9f6f29
RaddN <hossainraihan02@gmail.com>
2026-04-09 16:31:08 +0600

    added cart recovery

--- includes/documentation.php
+++ includes/documentation.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) exit;
 /**
  * Renders the Floating Cart documentation page
  * 
- * @since 1.1.7.4.6
+ * @since 1.1.9.20
  * @return void
  */
 function onepaqucpro_cart_documentation()
